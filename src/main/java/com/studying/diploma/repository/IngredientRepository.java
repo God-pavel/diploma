20a23eb267c76b8492c1a80547b759b1e1aaf1a4
Pavlo Pankratov <p.p.1999027@gmail.com>
2021-04-29 15:12:09 +0300

    added repositories

---
+++ src/main/java/com/studying/diploma/repository/IngredientRepository.java
@@ -0,0 +1,13 @@
+package com.studying.diploma.repository;
+
+
+import com.studying.diploma.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+}
+
+
---
+++ src/main/java/com/studying/diploma/repository/RecipeRepository.java
@@ -0,0 +1,13 @@
+package com.studying.diploma.repository;
+
+
+import com.studying.diploma.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
+
+
